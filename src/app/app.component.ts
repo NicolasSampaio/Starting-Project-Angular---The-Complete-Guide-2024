b59621050968986c594b3b7b0d72906a834ae46a
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2024-09-04 20:24:55 -0300

    ADD: Task component e Tasks layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS, User } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, CommonModule],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
