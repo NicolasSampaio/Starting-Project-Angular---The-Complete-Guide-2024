f3c8e9830ebd7d0ad5f91c48745dc32f4b517bbd
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2024-09-04 20:32:31 -0300

    ADD: Tasks específicas para cada usuário

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { dummyTasks } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -9,5 +10,11 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) name!: string;
+  tasks = dummyTasks;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.id);
+  }
 }
