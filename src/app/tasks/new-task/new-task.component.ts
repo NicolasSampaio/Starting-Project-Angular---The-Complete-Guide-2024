d98e6d1d076a9aa462fb11a1164431e355ef80d9
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2024-09-05 11:24:58 -0300

    ADD: Ngmodel

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancel: EventEmitter<void> = new EventEmitter();
   @Output() create: EventEmitter<void> = new EventEmitter();
+
+  title = '';
+  summary = '';
+  date = '';
 }
