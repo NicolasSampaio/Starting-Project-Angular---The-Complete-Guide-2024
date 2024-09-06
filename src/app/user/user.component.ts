2e6b47dbea8e53d1060c8f971ea58ae1b2ce7633
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2024-09-06 17:42:54 -0300

    ADD: Using ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
