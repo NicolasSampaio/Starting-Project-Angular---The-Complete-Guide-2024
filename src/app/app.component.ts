5d11c5fb33905e08bfe23bfbcab3e4cb7313b238
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2024-09-08 21:01:44 -0300

    ADD: Use modules with standalones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { User } from './user/user.model';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
 
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
